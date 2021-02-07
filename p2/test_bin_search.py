912e219d263c9a38ff02828787565c1e7e33b577
Mstrutov <strutovsky.m.a@gmail.com>
2021-02-07 18:22:15 +0300

    fixed unsorted array test

--- p2/test_bin_search.py
+++ p2/test_bin_search.py
@@ -37,18 +37,17 @@ def test_right_absent_element():
     assert bin_search([1,2,3,5,6,7], 8) == 6
 
 
-def test_many_equals_middle():
+def test_many_equal_elements_middle():
     assert bin_search([1,2,3,3,3,4,5,6], 3) == 5
 
 
-def test_many_equals_left():
+def test_many_equal_elements_left():
     assert bin_search([1,1,1,1,2,3,3,3,4,5,6], 1) == 4
 
 
-def test_many_equals_right():
+def test_many_equal_elements_right():
     assert bin_search([1,2,3,3,3,4,5,6,7,7,7,7], 7) == 12
 
 
-def test_unsorted_array_returns_some_index():
-    assert bin_search([1,6,2,3,5], 4) <= 5
-    assert bin_search([1, 6, 2, 3, 5], 4) >= 0
+def test_unsorted_array_yields_arbitrary_result():
+    assert bin_search([1,6,2,3,5], 4) == 4
